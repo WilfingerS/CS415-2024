b9c35d9bd679da8ef675eaee64ac5b07906cc196
Deaconpiglet <kaiba06010@gmail.com>
2024-06-12 12:00:44 -0400

    Created Interaction Maneger

--- GodotProjectFolder/Scenes/Levels/chest.gd
+++ GodotProjectFolder/Scenes/Levels/chest.gd
@@ -1,9 +1,14 @@
 extends Node2D
 
-var player = null
+@onready var interaction_area: InteractionArea = $InteractionArea
 
-func _on_detection_area_body_entered(body):
-	player = body
+const lines: Array[String] = [
+	"Hey Im a Cheast!"
+]
+
+func _ready():
+	interaction_area.interact = Callable(self, "_on_interact")
 	
-func _on_detection_area_body_exited(_body):
-	player = null
+	
+func _on_interact():
+	print(lines[0])
