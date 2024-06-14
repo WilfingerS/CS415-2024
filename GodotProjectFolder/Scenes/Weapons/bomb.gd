a0308811f3b8480e1f12c51a3813d91f9955d479
Seth Wilfinger <wilfingers@my.ccsu.edu>
2024-06-14 16:05:39 -0400

    updated bomb
     to destroy itself for memory

--- GodotProjectFolder/Scenes/Weapons/bomb.gd
+++ GodotProjectFolder/Scenes/Weapons/bomb.gd
@@ -10,9 +10,16 @@ var damage:int = 2
 		#print(self.position)
 		#self.position = player.position
 		#$AnimationPlayer.play("explosion")
+func _on_animation_finished(anim_name):
+	print(anim_name)
+	if anim_name == "explosion":
+		queue_free()
 
 func explode():
 	# Known bug is player is getting damaged as soon as its placed?
 	self.position = player.position + Vector2(0,-15)
-	
 	$AnimationPlayer.play("explosion")
+	
+func _ready():
+	# Connect to the animation finished signal
+	$AnimationPlayer.connect("animation_finished", self._on_animation_finished)
