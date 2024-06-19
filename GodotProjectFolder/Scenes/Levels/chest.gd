34b9f8283c14cc142a413122317ac2d94882313c
Deaconpiglet <kaiba06010@gmail.com>
2024-06-19 17:12:23 -0400

    added key and chest

--- GodotProjectFolder/Scenes/Levels/chest.gd
+++ GodotProjectFolder/Scenes/Levels/chest.gd
@@ -1,7 +1,9 @@
 extends Node2D
 
 @onready var interaction_area: InteractionArea = $InteractionArea
+@onready var animation = $AnimationPlayer
 @onready var Inv = []
+var scene_to_instance = preload("res://Scenes/Levels/Key.tscn")
 
 const lines: Array[String] = [
 	"Hey Im a Cheast!"
@@ -12,4 +14,10 @@ func _ready():
 	
 	
 func _on_interact():
-	print("Testing F")
+	if Global.Keys != 0:
+		get_node("InteractionArea/CollisionShape2D").disabled = true
+		animation.play("Open")
+		print(Global.Keys)
+		Global.Keys -= 1
+		print(Global.Keys)
+
