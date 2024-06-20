e6e4de51d40576eb0b93e9a6ad947ffe807433c6
Deaconpiglet <kaiba06010@gmail.com>
2024-06-20 00:07:20 -0400

    sprint

--- GodotProjectFolder/Scenes/Levels/chestNoKey.gd
+++ GodotProjectFolder/Scenes/Levels/chestNoKey.gd
@@ -8,9 +8,10 @@ func _ready():
 	interaction_area.interact = Callable(self, "_on_interact")
 	
 func _drop_key():
-	var item = Path.instantiate()
-	owner.add_child(item)
-	item.global_position = self.global_position + Vector2(0, 15)
+	if Path:
+		var item = Path.instantiate()
+		owner.add_child(item)
+		item.global_position = self.global_position + Vector2(0, 15)
 	
 func _on_interact():
 		get_node("InteractionArea/CollisionShape2D").disabled = true
