6f11281eb1a5779bb3d8b6952c7b3f0d79295d06
Seth Wilfinger <wilfingers@my.ccsu.edu>
2024-06-26 22:04:14 -0400

    boss door

--- GodotProjectFolder/BOSSROOM.gd
+++ GodotProjectFolder/BOSSROOM.gd
@@ -4,8 +4,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Global.door = door
-
-	
+	Global.door.hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
