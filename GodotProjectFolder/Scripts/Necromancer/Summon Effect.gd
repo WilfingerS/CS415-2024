d18416c7df0df235b3e961199b0404f8e5fae0e7
Seth Wilfinger <130659278+WilfingerS@users.noreply.github.com>
2024-06-27 07:06:46 -0400

    Delete GodotProjectFolder directory
    Purging Main to hold the Exe and .pck

--- GodotProjectFolder/Scripts/Necromancer/Summon Effect.gd
+++
@@ -1,7 +0,0 @@
-extends Sprite2D
-
-func _ready():
-	$AnimationPlayer.play("Arise")
-	var duration = $AnimationPlayer.current_animation_length +.1
-	await get_tree().create_timer(duration).timeout
-	queue_free()
